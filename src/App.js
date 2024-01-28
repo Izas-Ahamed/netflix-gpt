b6399a352cfcda3946f605fe1aa850fad5e87146
THUNDERX <thuderxcodes@gmail.com>
2024-01-28 16:23:48 +0530

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className=" font-bold text-blue-300 text-3xl text-center">
-      Hello from React!
+    <div>
+      <Body />
     </div>
   );
 }
